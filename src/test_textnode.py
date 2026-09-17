86e85740abe308f1f2673a1491d47c736aed743c
gingie-code <tywilsondawkins@gmail.com>
2026-09-16 23:38:32 -0700

    Add text_node_to_html_node conversion function and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -15,11 +15,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertEqual(node, node2)
 
-    #two nodes with the same text and type but different urls should not be equal
+   # two nodes with the same text and type but different urls should not be equal
     def test_eq_with_url(self):
         node = TextNode("Click here", TextType.LINK, "https://www.boot.dev")
-        node2 = TextNode("Click here", TextType.LINK, "https://www.boot.dev")
-        self.assertEqual(node, node2)
+        node2 = TextNode("Click here", TextType.LINK, "https://www.google.com")
+        self.assertNotEqual(node, node2)
 
     #Different text content should not make nodes equal
     def test_not_eq_different_text(self):
@@ -52,7 +52,63 @@ class TestTextNode(unittest.TestCase):
             repr(node),
             "TextNode(This is some anchor text, link, https://www.boot.dev)",
         )
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    # Plain text: no tag, raw value
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    # Bold: "b" tag
+    def test_bold(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text")
+
+    # Italic: "i" tag
+    def test_italic(self):
+        node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic text")
+
+    # Code: "code" tag
+    def test_code(self):
+        node = TextNode("print('hi')", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "print('hi')")
+
+    # Link: "a" tag, anchor text as value, href prop set from url
+    def test_link(self):
+        node = TextNode("Click me!", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me!")
+        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
+
+    # Image: "img" tag, empty value, src/alt props set from url/text
+    def test_image(self):
+        node = TextNode("A cool picture", TextType.IMAGE, "https://www.boot.dev/img.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev/img.png", "alt": "A cool picture"},
+        )
+
+    # Full rendering check, not just the intermediate LeafNode fields
+    def test_link_to_html(self):
+        node = TextNode("Click me!", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(
+            html_node.to_html(), '<a href="https://www.boot.dev">Click me!</a>'
+        )
 
 
 if __name__ == "__main__":
     unittest.main()
+
